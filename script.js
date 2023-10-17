260aa094b435c975c310f0b24c9967017c7f9a71
Sunmukh <sunmukhsc@gmail.com>
2023-10-16 20:21:11 -0700

    Add function that loops array to display books

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const myLibrary = [];
 
+const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true)
+const theFellowship = new Book("The Fellowship of the Ring", "J.R.R. Tolkien", 423, false)
+const theTwoTowers = new Book("The Two Towers", "J.R.R. Tolkien", 352, false)
+const theReturn = new Book("The Return of the King", "J.R.R. Tolkien", 416, false)
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -12,7 +17,18 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary(book) {
-  // do stuff here
+  myLibrary.push(book);
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false)
+function displayLibrary() {
+    for (book of myLibrary) {
+        console.log(book);
+    }
+}
+
+addBookToLibrary(theHobbit);
+addBookToLibrary(theFellowship);
+addBookToLibrary(theTwoTowers);
+addBookToLibrary(theReturn);
+
+displayLibrary();
